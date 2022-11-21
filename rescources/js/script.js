eefd67b4ae24b9ccc706f4c36c447cfdf0b676c0
TheDude1913 <79755365+TheDude1913@users.noreply.github.com>
2022-11-21 12:48:39 -0600

    calculator page thingy

---
+++ rescources/js/script.js
@@ -0,0 +1,103 @@
+var num1
+var num2
+
+var hasNum1 = false;
+
+var $screen = $("#screen");
+var $number = $(".number");
+
+var sign
+
+$number.on('click', function()
+{
+    var numberPressed = $(this).html();
+    $screen.append(numberPressed);
+}
+);
+
+document.getElementById("num1").onclick = function Write1 ()
+{
+    $screen.append("1");
+}
+document.getElementById("num2").onclick = function Write2 ()
+{
+    $screen.append("2");
+}
+
+document.getElementById("num3").onclick = function Write3 ()
+{
+    $screen.append("3");
+}
+
+document.getElementById("num4").onclick = function Write4 ()
+{
+    $screen.append("4");
+}
+
+document.getElementById("num5").onclick = function Write5 ()
+{
+    $screen.append("5");
+}
+
+document.getElementById("num6").onclick = function Write6 ()
+{
+    $screen.append("6");
+}
+document.getElementById("num7").onclick = function Write7 ()
+{
+    $screen.append("7");
+}
+
+document.getElementById("num8").onclick = function Write8 ()
+{
+    $screen.append("8");
+}
+
+document.getElementById("num9").onclick = function Write9 ()
+{
+    $screen.append("9");
+}
+
+document.getElementById("num0").onclick = function Write0 ()
+{
+    $screen.append("0");
+}
+
+document.getElementById("plus").onclick = function WritePlus ()
+{
+    $screen.append("+");
+    sign = "+"
+    $screen.append(num1);
+}
+
+document.getElementById("minus").onclick = function WritePlus ()
+{
+    $screen.append("-");
+    sign = "-"
+}
+
+document.getElementById("times").onclick = function WritePlus ()
+{
+    $screen.append("*");
+    sign = "*"
+}
+
+document.getElementById("devided").onclick = function WritePlus ()
+{
+    $screen.append("/");
+    sign = "/"
+}
+
+document.getElementById("equals").onclick = function WriteEquals ()
+{
+    $screen.append("=");
+    
+    
+}
+
+
+
+document.getElementById("clear").onclick = function Clear ()
+{
+    $screen.empty();
+}
---
+++ script.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Html;
+using static System.Console;
+using static System.Text.Encodings.Web.HtmlEncoder;
+
+
+namespace script
+{
+    
+}
